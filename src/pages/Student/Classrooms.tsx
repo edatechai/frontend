993f8aa3ec7eca9c8a87c706f640eadd588bc25d
NaijasonEzeiru <jayseehe1035@gmail.com>
2024-09-04 19:04:15 +0100

    ....................

--- src/pages/Student/Classrooms.tsx
+++ src/pages/Student/Classrooms.tsx
@@ -2,12 +2,57 @@ import { Link } from "react-router-dom";
 import { Users } from "lucide-react";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { useFindMyClassesQuery } from "../../features/api/apiSlice";
+import {
+  useFindMyClassesQuery,
+  useGetAllClassRoomByAccountIdQuery,
+  useGetQuizResultByUserIdQuery,
+} from "../../features/api/apiSlice";
 import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
 
 export function StudentClassrooms() {
   const userInfo = useSelector((state) => state.user.userInfo);
+  const { data: classes } = useGetAllClassRoomByAccountIdQuery(
+    userInfo?.accountId
+  );
   const { data: myClasses } = useFindMyClassesQuery(userInfo._id);
+  const {
+    data: quizResult,
+    isLoading: quizResultLoading,
+    isSuccess: quizResultSuccess,
+    isError: quizResultError,
+  } = useGetQuizResultByUserIdQuery(userInfo._id);
+  const dialogRef = useRef(null);
+  const [classRoom, setClassRoom] = useState();
+
+  const handleSubmit = async () => {
+    console.log("this is data", classRoom);
+    // filter classes where clesses id is classRoom
+    const filteredClasses = classes?.filter((item) => item._id === classRoom);
+    console.log("filtered classes", filteredClasses[0]);
+    const payload = {
+      classId: filteredClasses[0]._id,
+      ...userInfo,
+    };
+    console.log(payload);
+
+    try {
+      const response = await joinClass(payload);
+      response;
+      console.log(response);
+      if (response.data.status === false) {
+        dialogRef.current.close();
+        return alert(response.data.message);
+      }
+
+      if (response.data.status === true) {
+        dialogRef.current.close();
+        return alert(response.data.message);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   return (
     <div className="min-h-screen w-full flex flex-1 flex-col gap-4 md:gap-8">

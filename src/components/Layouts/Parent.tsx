87dd563e15ce94a9c920d53bdaac7520746e42cc
NaijasonEzeiru <jayseehe1035@gmail.com>
2024-09-21 17:03:35 +0100

    Added: Text2Speech to Chatbot

--- src/components/Layouts/Parent.tsx
+++ src/components/Layouts/Parent.tsx
@@ -1,24 +1,60 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation, useNavigate } from "react-router-dom";
 import {
-  Settings,
   Home,
-  User,
-  Users,
   LineChart,
   Menu,
   Circle,
+  Dumbbell,
+  StickyNote,
 } from "lucide-react";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
-
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Header from "../others/Header";
-import { useGetAccountByIdQuery } from "@/features/api/apiSlice";
-import { useSelector } from "react-redux";
+import {
+  useGetAccountByIdQuery,
+  useGetAllChildrenQuery,
+} from "@/features/api/apiSlice";
 
 export function ParentsLayout() {
   const userInfo = useSelector((state) => state.user.userInfo);
+  const { data: children, isLoading } = useGetAllChildrenQuery(
+    userInfo?.childrenArray
+  );
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
   const { data } = useGetAccountByIdQuery(userInfo.accountId);
+  const [wardId, setWardId] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (children?.length == 1) {
+      setWardId(children[0].user._id);
+    }
+  }, [children]);
+
+  useEffect(() => {
+    if (wardId == null) {
+      navigate("/parent");
+    }
+    if (pathname.startsWith("/parent/result")) {
+      navigate(`/parent/result/${wardId}`);
+    }
+    if (pathname.startsWith("/parent/strengths")) {
+      navigate(`/parent/strengths/${wardId}`);
+    }
+  }, [wardId]);
+
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-primary md:block">
@@ -46,10 +82,10 @@ export function ParentsLayout() {
                 <Home className="h-4 w-4" />
                 Dashboard
               </NavLink>
-              <NavLink
+              {/* <NavLink
                 to="/parent/report"
                 className={({ isActive }) =>
-                  `flex items-center gap-3 rounded-lg px-3 py-2 mt-2 transition-all ${
+                  `flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${
                     isActive
                       ? "bg-muted text-foreground hover:bg-slate-200"
                       : "text-primary-foreground hover:bg-blue-500"
@@ -58,12 +94,61 @@ export function ParentsLayout() {
               >
                 <LineChart className="h-4 w-4" />
                 Report
-              </NavLink>
+              </NavLink> */}
+              {!!wardId && (
+                <>
+                  <NavLink
+                    to={`/parent/result/${wardId}`}
+                    className={({ isActive }) =>
+                      `flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${
+                        isActive
+                          ? "bg-muted text-foreground hover:bg-slate-200"
+                          : "text-primary-foreground hover:bg-blue-500"
+                      }`
+                    }
+                  >
+                    <StickyNote className="h-4 w-4" />
+                    Result
+                  </NavLink>
+                  <NavLink
+                    to={`/parent/strengths/${wardId}`}
+                    className={({ isActive }) =>
+                      `flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${
+                        isActive
+                          ? "bg-muted text-foreground hover:bg-slate-200"
+                          : "text-primary-foreground hover:bg-blue-500"
+                      }`
+                    }
+                  >
+                    <Dumbbell className="h-4 w-4" />
+                    Strengths
+                  </NavLink>
+                </>
+              )}
             </nav>
           </div>
           <div className="mt-auto py-7 text-sm px-5 lg:px-7">
-            <p>EDAT</p>
-            <p>All Rights Reserved ©2024</p>
+            {children?.length > 1 && (
+              <Select onValueChange={(e) => setWardId(e)}>
+                <SelectTrigger className="bg-primary text-primary-foreground mb-5">
+                  <SelectValue placeholder="Select child" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Children</SelectLabel>
+                    {children?.map(({ user }) => (
+                      <SelectItem value={user?._id} key={user?._id}>
+                        {user?.fullName}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+            <span className="flex gap-2 text-xs items-center">
+              <img alt="" src="/edat_logo.png" className="w-12" />
+              <p>All Rights Reserved ©2024</p>
+            </span>
           </div>
         </div>
       </div>
@@ -101,6 +186,21 @@ export function ParentsLayout() {
                 </NavLink>
               </nav>
               <div className="mt-auto px-3">
+                {/* <Select>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select a fruit" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Fruits</SelectLabel>
+                      <SelectItem value="apple">Apple</SelectItem>
+                      <SelectItem value="banana">Banana</SelectItem>
+                      <SelectItem value="blueberry">Blueberry</SelectItem>
+                      <SelectItem value="grapes">Grapes</SelectItem>
+                      <SelectItem value="pineapple">Pineapple</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select> */}
                 <p>EDAT</p>
                 <p>All Rights Reserved ©2024</p>
               </div>
